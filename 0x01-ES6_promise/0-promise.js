9195023d7b3d4c51a26ffe52dcfa835526bb8661
NEAZYIT <touihnazih28@gmail.com>
2024-03-06 23:06:27 +0100

    Implemented getResponseFromAPI function to simulate API call with Promises

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,16 +1,5 @@
-// 0-promise.js
-
-function getResponseFromAPI() {
-    // Create and return a new Promise
-    return new Promise((resolve, reject) => {
-      // Your asynchronous logic or API call goes here
-      // For simplicity, let's resolve the promise after a short delay
-      setTimeout(() => {
-        // Simulating a successful API response
-        resolve({ data: "API response data" });
-      }, 1000);
-    });
-  }
+const getFullResponseFromAPI = (success) => new Promise((resolve, reject) => {
+    success ? resolve({ status: 200, body: 'Success' }) : reject(new Error('The fake API is not working currently'));
+  });
   
-  // Export the function
-  export default getResponseFromAPI;  
+  export default getFullResponseFromAPI;  
