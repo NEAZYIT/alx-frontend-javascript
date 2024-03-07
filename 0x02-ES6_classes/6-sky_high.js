624809d457317bad5d41075fb6efa009f2880e3b
NEAZYIT <touihnazih28@gmail.com>
2024-03-07 13:53:14 -0800

    Here, SkyHighBuilding extends the Building class, and the constructor assigns the sqft attribute to the parent class using super(sqft). The floors attribute is specific to the SkyHighBuilding class and is stored as _floors. Getter methods are implemented for both floors and sqft, and the evacuationWarningMessage method is overridden to provide the desired message.

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -1,23 +1,23 @@
 import Building from './5-building';
 
 export default class SkyHighBuilding extends Building {
-  constructor(sqft, floors) {
+  constructor (sqft, floors) {
     super(sqft);
     this.floors = floors;
   }
 
-  get floors() {
+  get floors () {
     return this._floors;
   }
 
-  set floors(newFloors) {
+  set floors (newFloors) {
     if (typeof newFloors !== 'number') {
       throw TypeError('Floors must be a number');
     }
     this._floors = newFloors;
   }
 
-  evacuationWarningMessage() {
+  evacuationWarningMessage () {
     return `Evacuate slowly the ${this._floors} floors`;
   }
 }
