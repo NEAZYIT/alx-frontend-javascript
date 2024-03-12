566d00b7012d464b42fd9cc12c736f9da16e3b54
NEAZYIT <touihnazih28@gmail.com>
2024-03-12 16:11:19 -0700

    This commit introduces the getListStudents function, which returns an array of objects representing students. Each student object contains three attributes: id, firstName, and location. The array includes the following students: Guillaume in San Francisco (id: 1), James in Columbia (id: 2), and Serena in San Francisco (id: 5).

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,15 +1,7 @@
-// 0-get_list_students.js
-
-/**
- * Returns an array of objects representing students.
- * @returns {Array} Array of student objects.
- */
-function getListStudents() {
-    return [
-        { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-        { id: 2, firstName: 'James', location: 'Columbia' },
-        { id: 5, firstName: 'Serena', location: 'San Francisco' }
-    ];
+export default function getListStudents() {
+  return [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
 }
-
-export default getListStudents;
